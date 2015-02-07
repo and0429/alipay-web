173e33b4a3002924052377bc7c75905e6f5e40e1
and0429 <zk616117>
2015-02-07 10:38:42 +0800

    user

--- alipay-web/src/main/java/com/collect/alipay/web/UserController.java
+++ alipay-web/src/main/java/com/collect/alipay/web/UserController.java
@@ -1,8 +1,13 @@
 package com.collect.alipay.web;
 
+import javax.inject.Inject;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.collect.alipay.domain.User;
+import com.collect.alipay.service.UserService;
 
 /**
  * 用户处理类
@@ -14,10 +19,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+	@Inject
+	private UserService userService;
+
+	/**
+	 * 查询所有列表
+	 * 
+	 * @return 列表对象
+	 */
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
 	public Object users() {
 
-		return null;
+		return userService.getAll();
 
+	}
+
+	/**
+	 * 添加一个实体
+	 * 
+	 * @param user
+	 */
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public void add(User user) {
+
+		userService.save(user);
 	}
 }
